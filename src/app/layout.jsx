f14861f5db522a206f7a49170c7b26f37e53188e
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2025-03-12 00:02:52 +0200

    nixua ne poluchilos

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -94,10 +94,10 @@ export default function RootLayout({ children }) {
           crossOrigin="use-credentials"
           href="/site.webmanifest"
         />
-        {/* <link
+        <link
           href="https://fonts.googleapis.com/css2?family=Bruno+Ace&family=Comfortaa:wght@300;400;500;600;700&family=Lato&family=Montserrat:wght@400;500;600&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </head>
       <body>
         <div className="page">
